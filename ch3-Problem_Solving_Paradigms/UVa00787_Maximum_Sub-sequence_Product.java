43fb5d99932abeae4b3689e77d46107ebe955f79
SpongeVincent <SpongeVincent@users.noreply.github.com>
2016-02-19 11:29:13 +1300

    Create UVa00787_Maximum_Sub-sequence_Product.java

---
+++ ch3-Problem_Solving_Paradigms/UVa00787_Maximum_Sub-sequence_Product.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(
+                new OutputStreamWriter(System.out)));
+    }
+}
