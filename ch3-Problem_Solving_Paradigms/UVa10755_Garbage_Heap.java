9ba4ebcf8be57e11b075d539f9bd96fd7ad15114
SpongeVincent <SpongeVincent@users.noreply.github.com>
2016-02-26 17:16:59 +1300

    Update UVa10755_Garbage_Heap.java

--- ch3-Problem_Solving_Paradigms/UVa10755_Garbage_Heap.java
+++ ch3-Problem_Solving_Paradigms/UVa10755_Garbage_Heap.java
@@ -1,53 +1,46 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(new BufferedWriter(
-                new OutputStreamWriter(System.out)));
-        int caseNum = Integer.parseInt(in.readLine());
+        Scanner in = new Scanner(System.in);
+        int caseNum = in.nextInt();
         for (int t = 0; t < caseNum; t++) {
-            StringTokenizer st = new StringTokenizer(in.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
+            int A = in.nextInt();
+            int B = in.nextInt();
+            int C = in.nextInt();
             long[][][] cube = new long[C][A][B];
-            st = new StringTokenizer(in.readLine());
             for (int a = 0; a < A; a++) {
                 for (int b = 0; b < B; b++) {
                     for (int c = 0; c < C; c++) {
-                        cube[c][a][b] = Integer.parseInt(st.nextToken());
+                        cube[c][a][b] = in.nextLong();
                     }
                 }
             }
-            out.println(kadane3D(cube));
-            if (t < caseNum - 1) out.println();
+            System.out.println(kadane3D(cube));
+            if (t < caseNum - 1) System.out.println();
         }
-        out.flush();
-        out.close();
     }
 
-    public static long kadane1D(long[] arr){
-        long currSum = 0, max = -9999999999l;
+    public static long kadane1D(long[] arr) {
+        long currSum = 0, max = Long.MIN_VALUE;
         for (long num : arr) {
             if (num + currSum >= num) {
                 currSum += num;
                 max = currSum > max ? currSum : max;
-            }
-            else currSum = num;
+            } else currSum = num;
         }
         max = currSum > max ? currSum : max;
         return max;
     }
 
     public static long kadane2D(long[][] arr) {
-        long max = -99999999999l;
-        for (int l = 0; l < arr[0].length; l++ ) {
+        long max = Long.MIN_VALUE;
+        for (int l = 0; l < arr[0].length; l++) {
             long[] temp = new long[arr.length];
             for (int r = l; r < arr[0].length; r++) {
+                /* increment of temp arr */
                 for (int i = 0; i < arr.length; i++) temp[i] += arr[i][r];
                 long rst1D = kadane1D(temp);
                 max = rst1D > max ? rst1D : max;
@@ -57,10 +50,12 @@ public class Main{
     }
 
     public static long kadane3D(long[][][] arr) {
-        long max = -99999999999l;
+        /* return value: int rst = max */
+        long max = Long.MIN_VALUE;
         for (int cUp = 0; cUp < arr.length; cUp++) {
             long temp[][] = new long[arr[0].length][arr[0][0].length];
             for (int cDown = cUp; cDown < arr.length; cDown++) {
+                /* increment of temp arr */
                 for (int a = 0; a < arr[0].length; a++) {
                     for (int b = 0; b < arr[0][0].length; b++) {
                         temp[a][b] += arr[cDown][a][b];
@@ -72,3 +67,40 @@ public class Main{
         }
         return max;
     }
+}
+
+//    public static int[] kadane1D(int[] arr){
+//        /* return value : int[] rst = {max. start(up), end(down) */
+//        int currSum = 0, max = 0, start = 0, end = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] + currSum > arr[i]) {
+//                currSum += arr[i];
+//                if (currSum > max) {
+//                    max = currSum;
+//                    end = i;
+//                }
+//            }
+//            else {
+//                currSum = arr[i];
+//                start = i;
+//            }
+//        }
+//        return new int[]{max, start, end};
+//    }
+
+//    public static int[] kadane2D(int[][] arr) {
+//        /* return value: int[] rst = {max, left, right, up, down} */
+//        int max = 0, left = 0, right = 0, up = 0, down = 0;
+//        for (int l = 0; l < arr[0].length; l++ ) {
+//            int[] temp = new int[arr.length];
+//            for (int r = l; r < arr[0].length; r++) {
+//                /* increment of temp arr */
+//                for (int i = 0; i < arr.length; i++) temp[i] += arr[i][r];
+//                int[] rst1D = kadane1D(temp);
+//                if (rst1D[0] > max) {
+//                    max = rst1D[0]; left = l; right = r; up = rst1D[1]; down = rst1D[2];
+//                }
+//            }
+//        }
+//        return new int[]{max, left, right, up, down};
+//    }
