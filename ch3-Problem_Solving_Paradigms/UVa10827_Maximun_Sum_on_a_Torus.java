aa81a33e4beef1db588c8aa9ce6f142bf6c4582a
SpongeVincent <SpongeVincent@users.noreply.github.com>
2016-02-29 16:42:34 +1300

    By using Kadane's algorithm
    First convert the 2D problem into 1D problem, then use Kadane's algorithm to solve it. Complexity is O(n^4).

--- ch3-Problem_Solving_Paradigms/UVa10827_Maximun_Sum_on_a_Torus.java
+++ ch3-Problem_Solving_Paradigms/UVa10827_Maximun_Sum_on_a_Torus.java
@@ -5,7 +5,6 @@ public class UVa10827 {
 
     static int size;
     public static void main(String[] args) throws IOException {
-        long startTime = System.currentTimeMillis();
         Scanner in = new Scanner(System.in);
         int caseNum = in.nextInt();
         for (int t = 0; t < caseNum; t++) {
@@ -13,20 +12,18 @@ public class UVa10827 {
             int[][] board = new int[size * 2][size * 2];
             for (int a = 0; a < size; a++) {
                 for (int b = 0; b < size; b++) {
-                    board[b][a] = board[b + size][a] = board[b][a + size] = in.nextInt();
+                    board[b][a] = board[b + size][a] = board[b][a + size]
+                            = board[b + size][a + size] = in.nextInt();
                 }
             }
-            startTime = System.currentTimeMillis();
             System.out.println(kadane2D(board));
         }
-        final long endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime - startTime) / 1000.0);
     }
 
     public static int kadane1D(int[] arr) {
-        int currSum = 0, max = -1000;
+        int max = -1000;
         for (int start = 0; start < size; start++) {
-            currSum = 0;
+            int currSum = 0;
             for (int i = start; i < start + size; i++) {
                 if (arr[i] + currSum >= arr[i]) {
                     currSum += arr[i];
@@ -43,10 +40,10 @@ public class UVa10827 {
     public static int kadane2D(int[][] arr) {
         int max = -1000;
         for (int l = 0; l < size; l++) {
-            int[] temp = new int[size * 2 - 1];
+            int[] temp = new int[arr.length];
             for (int r = l; r < l + size; r++) {
             /* increment of temp arr */
-                for (int i = 0; i < size * 2 - 1; i++) temp[i] += arr[r][i];
+                for (int i = 0; i < arr.length; i++) temp[i] += arr[r][i];
                 int rst1D = kadane1D(temp);
                 max = rst1D > max ? rst1D : max;
             }
