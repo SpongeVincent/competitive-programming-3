e316af5d1da859d0a0a09b0cb8d2b6f83f346636
SpongeVincent <SpongeVincent@users.noreply.github.com>
2016-03-02 16:52:54 +1300

    Can use Kadane's alg or summed area table
    Using Kadane's algorithm leas to TL, even though they gets the same compexity which is O(n^5)...  i think

--- ch3-Problem_Solving_Paradigms/UVa10755_Garbage_Heap.java
+++ ch3-Problem_Solving_Paradigms/UVa10755_Garbage_Heap.java
@@ -1,30 +1,71 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
+    static int A, B, C;
+    static long max;
 
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
         int caseNum = in.nextInt();
         for (int t = 0; t < caseNum; t++) {
-            int A = in.nextInt();
-            int B = in.nextInt();
-            int C = in.nextInt();
-            long[][][] cube = new long[C][A][B];
-            for (int a = 0; a < A; a++) {
-                for (int b = 0; b < B; b++) {
-                    for (int c = 0; c < C; c++) {
-                        cube[c][a][b] = in.nextLong();
+            A = in.nextInt() + 1;
+            B = in.nextInt() + 1;
+            C = in.nextInt() + 1;
+            long[][][] cube = new long[A][B][C];
+            for (int a = 1; a < A; a++) {
+                for (int b = 1; b < B; b++) {
+                    for (int c = 1; c < C; c++) {
+                        cube[a][b][c] = in.nextLong();
+
+                        /* reconstruct summed space table */
+                        /* --------------------------------- */
+                        cube[a][b][c] += cube[a][b - 1][c] + cube[a][b][c - 1] - cube[a][b - 1][c -1]
+                                         + cube[a - 1][b][c] - cube[a - 1][b - 1][c] - cube[a - 1][b][c - 1]
+                                         + cube[a - 1][b - 1][c - 1];
+                        /* --------------------------------- */
                     }
                 }
             }
-            System.out.println(kadane3D(cube));
+            max = Long.MIN_VALUE;
+            /* using Kadane's algorithm is kinda slow for these question. complexity is O(n^5). */
+            /* --------------------------------- */
+//            kadane3D(cube);
+            /* --------------------------------- */
+
+
+            /* using summed space/area table will not get TL. complexity is O(n^5) */
+            /* --------------------------------- */
+            summedSpace(cube);
+            /* --------------------------------- */
+            System.out.println(max);
             if (t < caseNum - 1) System.out.println();
         }
     }
 
-    public static long kadane1D(long[] arr) {
-        long currSum = 0, max = Long.MIN_VALUE;
+    public static void summedSpace(long[][][] cube) {
+        for (int down = 1; down < A; down++) {
+            for (int up = down; up < A; up++) {
+
+                for (int front = 1; front < B; front++) {
+                    for (int back = front; back < B; back++) {
+
+                        long min = 0;
+                        for (int c = 1; c < C; c++) {
+                            long subSum = cube[up][back][c] - cube[down - 1][back][c]
+                                          - cube[up][front - 1][c]  + cube[down - 1][front - 1][c];
+                            max = Math.max(max, subSum - min);
+
+                            min = subSum < min ? subSum : min;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void kadane1D(long[] arr) {
+        long currSum = 0;
         for (long num : arr) {
             if (num + currSum >= num) {
                 currSum += num;
@@ -32,75 +73,32 @@ public class Main {
             } else currSum = num;
         }
         max = currSum > max ? currSum : max;
-        return max;
     }
 
-    public static long kadane2D(long[][] arr) {
-        long max = Long.MIN_VALUE;
+    public static void kadane2D(long[][] arr) {
         for (int l = 0; l < arr[0].length; l++) {
             long[] temp = new long[arr.length];
             for (int r = l; r < arr[0].length; r++) {
-                /* increment of temp arr */
+            /* increment of temp arr */
                 for (int i = 0; i < arr.length; i++) temp[i] += arr[i][r];
-                long rst1D = kadane1D(temp);
-                max = rst1D > max ? rst1D : max;
+                kadane1D(temp);
             }
         }
-        return max;
     }
 
-    public static long kadane3D(long[][][] arr) {
-        /* return value: int rst = max */
-        long max = Long.MIN_VALUE;
-        for (int cUp = 0; cUp < arr.length; cUp++) {
-            long temp[][] = new long[arr[0].length][arr[0][0].length];
-            for (int cDown = cUp; cDown < arr.length; cDown++) {
-                /* increment of temp arr */
-                for (int a = 0; a < arr[0].length; a++) {
-                    for (int b = 0; b < arr[0][0].length; b++) {
-                        temp[a][b] += arr[cDown][a][b];
-                        long currSum = kadane2D(temp);
-                        max = currSum > max ? currSum : max;
+    public static void kadane3D(long[][][] arr) {
+    /* return value: int rst = max */
+        for (int down = 1; down < A; down++) {
+            long temp[][] = new long[B][C];
+            for (int up = down; up < A; up++) {
+            /* increment of temp arr */
+                for (int b = 1; b < B; b++) {
+                    for (int c = 1; c < C; c++) {
+                        temp[b][c] += arr[up][b][c];
+                        kadane2D(temp);
                     }
                 }
             }
         }
-        return max;
     }
 }
-
-//    public static int[] kadane1D(int[] arr){
-//        /* return value : int[] rst = {max. start(up), end(down) */
-//        int currSum = 0, max = 0, start = 0, end = 0;
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] + currSum > arr[i]) {
-//                currSum += arr[i];
-//                if (currSum > max) {
-//                    max = currSum;
-//                    end = i;
-//                }
-//            }
-//            else {
-//                currSum = arr[i];
-//                start = i;
-//            }
-//        }
-//        return new int[]{max, start, end};
-//    }
-
-//    public static int[] kadane2D(int[][] arr) {
-//        /* return value: int[] rst = {max, left, right, up, down} */
-//        int max = 0, left = 0, right = 0, up = 0, down = 0;
-//        for (int l = 0; l < arr[0].length; l++ ) {
-//            int[] temp = new int[arr.length];
-//            for (int r = l; r < arr[0].length; r++) {
-//                /* increment of temp arr */
-//                for (int i = 0; i < arr.length; i++) temp[i] += arr[i][r];
-//                int[] rst1D = kadane1D(temp);
-//                if (rst1D[0] > max) {
-//                    max = rst1D[0]; left = l; right = r; up = rst1D[1]; down = rst1D[2];
-//                }
-//            }
-//        }
-//        return new int[]{max, left, right, up, down};
-//    }
